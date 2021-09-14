c08fd69e06aeab9c4230484792a1096a1e2bd4a8
hamdiRH <hamdi.rahal@gmail.com>
2021-09-14 12:26:36 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# recoilJS-expense-tracker
+
+## highly inspired by https://github.com/bradtraversy/vanillawebprojects/tree/master/expense-tracker
---
+++ src/recoil/atoms.js
@@ -0,0 +1,6 @@
+import { atom } from 'recoil'
+
+export const transactionListState = atom({
+    key: 'transactionListState',
+    default: [],
+})
